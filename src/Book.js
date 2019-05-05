e1155c9e1a156499255a6951cbb01dfb699f6a22
Hong Zhou <zhsnnu@gmail.com>
2019-05-04 23:30:45 -0500

    my first react app

---
+++ src/Book.js
@@ -0,0 +1,13 @@
+import React from 'react';
+export const Book = ({title="No Title Provided", author="No Author", pages=0, freeBookmark}) => {
+    return (
+        <section>
+            <h2>{title}</h2>
+            <p>By: {author}</p>
+            <p>Pages: {pages} pages</p>
+            <p>Free Bookmark Today: {freeBookmark ? 'yes!' : 'No!'}</p>
+        </section>
+    )
+}
+
+
